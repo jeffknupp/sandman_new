32a04f3ae97314300badb58c63998aa3cfc86fe9
Jeff Knupp <jeff@jeffknupp.com>
2014-06-01 19:33:29 -0400

    Add gitignore

--- scripts/runserver.py
+++ scripts/runserver.py
@@ -2,8 +2,6 @@
 from sqlalchemy.ext.automap import automap_base
 from sandman import app, reflect_all
 from sandman.models import db
-import models
-import os
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite+pysqlite:///existing.sqlite3'
 app.secret_key = 's3cr3t'
 db.init_app(app)
